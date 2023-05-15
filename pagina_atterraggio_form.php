7640bd667fb898cc704b0497bad91d60f3749ad7
nvaccina <niccolovac@gmail.com>
2023-05-15 15:16:21 +0200

    Pagina di atterraggio form, printed text & length

--- pagina_atterraggio_form.php
+++ pagina_atterraggio_form.php
@@ -1,5 +1,7 @@
 <?php
 
+$text_area = $_GET['text_area'];
+$parola = $_GET['name'];
 
 ?>
 
@@ -9,9 +11,21 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   <title>Pagina atterraggio form</title>
 </head>
 <body>
+  <div class="container my-5">
+    <h2>Questo è il testo inserito:</h2>
+    <p><?php echo $text_area ?></p>
+    <h4>Il testo che hai inserito è lungo <?php echo strlen($text_area)?> parole</h4>
+
+    <h3>La parola che vuoi censurare è:</h3>
+    <span><?php echo $parola ?></span>
+  </div>
+
   
 </body>
 </html>
